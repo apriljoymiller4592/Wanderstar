5993daea7507f136f574efc478edab78ae4eb72a
apriljoymiller4592 <113550415+apriljoymiller4592@users.noreply.github.com>
2024-02-23 13:27:03 -0800

    first commit

---
+++ Counts.py
@@ -0,0 +1,10 @@
+# CLASS TO COUNT NUMBER OF OCCURENCES OF EVENT PER SECOND
+# IMPORT NECESSARY LIBRARIES
+from datetime import datetime
+class Counts:
+
+    def __init__(self):
+        print("init")
+
+    def count_per_sec(self):
+        print("counter per sec")
---
+++ WanderStar.py
@@ -0,0 +1,63 @@
+# IMPORT NECESSARY LIBRARIES
+import sys
+import cv2
+from cv2 import *
+import numpy as np
+import math
+import os
+from os import listdir
+from os.path import isfile, join, isdir
+import time
+import datetime
+import threading
+from threading import Thread
+from Counts import Counts
+
+# INITIALIZE ARGUMENTS
+
+# CREATE AND TRAIN THE DEEP LEARNING MODEL
+def init_deep_learning():
+    print("training model")
+
+# CLASSIFY AN IMAGE
+def classify_image(img):
+    print("classify an image")
+
+# PERFORM A SPELL
+def perform_spell(spell):
+    print("performing spell")
+
+# CHECK FOR VALID GESTURE
+def check_for_gesture(movements, example_frame):
+    print("checking for gesture")
+
+# REMOVE THE BACKGROUND
+def remove_background():
+    print("removing background")
+
+# CALCULATE THE FRAME THRESHOLD
+def calc_frame_threshold():
+    print("calculating frame threshold")
+
+# PROCESS THE DATA
+def process_data():
+    print("processing data")
+
+# DISPLAY ITERATIONS PER SECOND
+def iterations_per_sec():
+    print("displaying iterations per second")
+
+
+# INITIATE CLASSIFICATION ALGORITHM
+init_deep_learning()
+
+# START NECESSARY THREADS TO PROCESS THE FRAMES
+remove_background()
+calc_frame_threshold()
+process_data()
+
+# CAPTURE THE VIDEO
+print("capturing video")
+
+# RUN THE MAIN LOOP
+print("running main loop")
