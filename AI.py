c4ae8cce5cfeb20f31e8658a64646cd412a1f671
apriljoymiller4592 <113550415+apriljoymiller4592@users.noreply.github.com>
2024-04-21 18:28:00 -0700

    Add files via upload

--- AI.py
+++ AI.py
@@ -11,18 +11,37 @@ from keras.models import load_model
 from keras.applications.mobilenet_v2 import preprocess_input
 from keras.preprocessing import image
 import tensorflow as tf
+
+import Music
 import Slides
 import bulb
+import Shelly as shelly
+from Music import MusicPlayer
+import threading
 
 print(tf.__version__)
 
 class_to_label = "Not_Loaded_Yet"
-#model = tf.keras.models.load_model('model/model6')
-model = tf.saved_model.load('model/model6')
+model = tf.keras.models.load_model('model/model18best.h5')
+#model = tf.saved_model.load('model/model6')
 
-infer = model.signatures['serving_default']
+shelly_ip1 = "192.168.1.116"
+shelly_ip2 = "192.168.1.117"
 
-converter = tf.lite.TFLiteConverter.from_saved_model('model/model6')
+#music, stop_event = Music.music_player(False)
+
+#stop_music_event = threading.Event()
+#music_thread = threading.Thread(target=music.listen(stop_music_event), args=(stop_music_event,))
+
+#music.listen(stop_event)
+
+#music.play_song(stop_music_event)
+
+
+
+'''infer = model.signatures['serving_default']
+
+converter = tf.lite.TFLiteConverter.from_saved_model('model/model8')
 #converter.optimizations = [tf.lite.Optimize.DEFAULT]
 tflite_model = converter.convert()
 
@@ -35,62 +54,56 @@ interpreter.allocate_tensors()
 
 input_tensor = tf.random.normal([1, 224, 224, 3], dtype=tf.float32)
 output = infer(input_tensor)
-
+'''
 def feed(inputPath):
-    img = image.load_img(inputPath, target_size=(224, 224), color_mode='rgb')  # Adjust size and color mode
+    img = image.load_img(inputPath, target_size=(224, 224), color_mode='grayscale')  # Adjust size and color mode
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)  # Create a batch
     img_array = img_array.astype('float32') / 255.0
 
     # Make a prediction
-#    predictions_main = model.predict(img_array)
+    predictions_main = model.predict(img_array)
 
-    # Flag to signify valid or invalid gesture
-    poopy_prediction = False
-    '''
-    #input_details = interpreter.get_input_details()
-    #output_details = interpreter.get_output_details()
-
-    input_tensor = tf.convert_to_tensor(img_array)
-    output = infer(input_tensor)
-
-    interpreter.set_tensor(input_tensor[0]['index'], img_array)
-    interpreter.invoke()
-    predictions = interpreter.get_tensor(output[0]['index'])
-'''
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
-
-    # Set the tensor to point to the input data to be inferred
-    interpreter.set_tensor(input_details[0]['index'], img_array)
-    interpreter.invoke()
-
-    # Get the prediction results
-    predictions = interpreter.get_tensor(output_details[0]['index'])
-    class_index = np.argmax(predictions, axis=1)
+    class_index = np.argmax(predictions_main, axis=1)
 
     with open('model/labels.json') as f:
         class_to_label = json.load(f)
     predicted_label = class_to_label[str(class_index[0])]
 
-    if np.max(predictions) < 0.95:
-        print("Poopy prediction")
-        poopy_prediction = True
+    if (np.max(predictions_main) < 0.9 or predicted_label == "Junk"
+            or predicted_label == ''):
         pass
     else:
         print(f"Feeding AI:  {inputPath}")
         print("This is the time to make a prediction.")
-        print(f"Predicted class: {predicted_label}, Confidence: {np.max(predictions)}")
+        print(f"Predicted class: {predicted_label}, Confidence: {np.max(predictions_main)}")
 
-        if predicted_label == 'Z':
+        if predicted_label == 'Square':
             bulb.switch("turn", "on")
+            bulb.change_color("color", 0, 0, 255)
+            time.sleep(1)
             bulb.change_color("color", 0, 255, 0)
-        elif predicted_label == 'Circle':
+            time.sleep(1)
+            bulb.change_color("color", 255, 0, 0)
+        elif predicted_label == 'Squiggle':
             PRESENTATION_ID = "1CricEs7HjbHiU-u3Tjz1jwQGpd2yq_AN61BhtI3YG24"
             viewer = Slides.GoogleSlidesViewer(PRESENTATION_ID)
         elif predicted_label == 'Triangle':
-            bulb.change_color("color", 0, 0, 255)
-        elif predicted_label == "Junk" or poopy_prediction:
-            bulb.change_color("color", 255, 0, 0)
-
-
+            bulb.switch("turn", "off")
+        elif predicted_label == 'Right Arrow':
+            shelly.turn_on_or_off(shelly_ip1, "on")
+        elif predicted_label == 'Left Arrow':
+            shelly.turn_on_or_off(shelly_ip1, "off")
+        elif predicted_label == 'U':
+            shelly.turn_on_or_off(shelly_ip2, "on")
+        elif predicted_label == 'Upside Down U':
+            shelly.turn_on_or_off(shelly_ip2, "off")
+        elif predicted_label == 'Star':
+            player = MusicPlayer()
+            time.sleep(1)
+            player.play_pause()
+        elif predicted_label == 'Circle':
+            print("Open box")
+        elif predicted_label == 'Z':
+            PRESENTATION_ID = "1CricEs7HjbHiU-u3Tjz1jwQGpd2yq_AN61BhtI3YG24"
+            viewer = Slides.GoogleSlidesViewer(PRESENTATION_ID).next()
