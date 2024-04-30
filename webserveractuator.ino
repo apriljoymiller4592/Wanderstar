c496497797025fb39d2129633d206c8c28bd50cc
tareun <tjongcharoeun@gmail.com>
2024-04-30 03:10:05 -0700

    Update webserveractuator.ino

--- webserveractuator.ino
+++ webserveractuator.ino
@@ -14,6 +14,8 @@ const int ENA_PIN = 9; // the Arduino pin connected to the EN1 pin L298N
 const int IN1_PIN = 6; // the Arduino pin connected to the IN1 pin L298N
 const int IN2_PIN = 5; // the Arduino pin connected to the IN2 pin L298N
 
+bool actuatorFlag = false;
+
 
 void setup() {
 
@@ -65,7 +67,7 @@ void loop() {
     // an HTTP request ends with a blank line
     boolean currentLineIsBlank = true;
     while (client.connected()) {
-      if (client.available()) {
+      if (client.available() && actuatorFlag == false) {
         char c = client.read();
         Serial.write(c);
         // if you've gotten to the end of the line (received a newline
@@ -101,18 +103,75 @@ void loop() {
           // you've gotten a character on the current line
           currentLineIsBlank = false;
         }
+
+        // give the web browser time to receive the data
+        //delay(10000);
+        delay(10);
+
+        // close the connection:
+        client.stop();
+        Serial.println("client disconnected");
+      }
+
+      else if(client.available() && actuatorFlag == true)
+      {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the HTTP request has ended,
+        // so you can send a reply
+        //offActuatorChange();
+        offActuatorChange();
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard HTTP response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          // output the value of each analog input pin
+          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+            int sensorReading = analogRead(analogChannel);
+            client.print("analog input ");
+            client.print(analogChannel);
+            client.print(" is ");
+            client.print(sensorReading);
+            client.println("<br />");
+          }
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+
+        // give the web browser time to receive the data
+        //delay(10000);
+        delay(10);
+
+        // close the connection:
+        client.stop();
+        Serial.println("client disconnected");
       }
     }
+    /*
     // give the web browser time to receive the data
-    delay(10000);
+    //delay(10000);
+    delay(10);
 
     // close the connection:
     client.stop();
     Serial.println("client disconnected");
     
     offActuatorChange();
-
-    delay(10000);
+    */
+    delay(10);
   }
 }
 
@@ -145,9 +204,12 @@ void onActuatorChange()  {
     //digitalWrite(IN1_PIN, LOW);
     //digitalWrite(IN2_PIN, HIGH);
   //}
+    actuatorFlag = true;
 }
 
 void offActuatorChange(){
   digitalWrite(IN1_PIN, LOW);
   digitalWrite(IN2_PIN, HIGH);
+
+  actuatorFlag = false;
 }
